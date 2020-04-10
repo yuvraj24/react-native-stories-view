21f54436383248c4bcb46ca24b9399a732978167
Yuvraj Pandey <yuvrajpandey24@gmail.com>
2020-04-10 15:23:27 +0530

    Gif Added

--- example/App.js
+++ example/App.js
@@ -15,20 +15,24 @@ import {StoryContainer, ProgressBar} from 'react-native-stories-view';
 // import StoryContainer from './src/stories/StoryContainer';
 // import ProgressBar from './src/stories/ProgressView';
 
-import {MID_GREEN, BLACK, LIGHT_GREEN, BAR_ACTIVE_COLOR, BAR_INACTIVE_COLOR, TINT_GRAY} from './colors';
+import {
+  MID_GREEN,
+  BLACK,
+  LIGHT_GREEN,
+  BAR_ACTIVE_COLOR,
+  BAR_INACTIVE_COLOR,
+  TINT_GRAY,
+} from './colors';
 
 import PROFILE from './images/profile2.jpg';
 import BACK from './images/back.png';
 
-const images = [ 
-  'https://s3.ap-south-1.amazonaws.com/hsdreams1/pins/2019/01/big/7d1e5e0b31a650b9314023921b9e161b.jpeg', 
+const images = [
+  'https://s3.ap-south-1.amazonaws.com/hsdreams1/pins/2019/01/big/7d1e5e0b31a650b9314023921b9e161b.jpeg',
   'https://boostupliving.com/wp-content/uploads/2019/06/best-motivational-quote-mobile-wallpapers-53.jpg',
   'https://i.pinimg.com/originals/51/bd/4c/51bd4c1e72d5d6ae5f2a4f31e31d2ef5.jpg',
   'https://pumpernickelpixie.com/wp-content/uploads/2016/01/15-phone-wallpaper.jpg',
   'https://i.pinimg.com/originals/5a/f0/e5/5af0e538f7437fd13a73f7c96601ccb6.jpg', 
-  // 'https://i.pinimg.com/originals/60/10/4e/60104ed3bdd932911991ead9611c3489.jpg', 
-  // 'https://hdwallpaper20.com/wp-content/uploads/2018/02/samsumg-wallpaper-mobile-x-PIC-WPPW3307-576x1024.jpg', 
-  // 'https://www.itl.cat/pngfile/big/0-7755_nature-pier-bridge-d-river-water-sunset-night.jpg',
 ];
 
 export default App = () => {
@@ -48,25 +52,35 @@ export default App = () => {
       {Platform.OS === 'android' && (
         <StatusBar barStyle="dark-content" backgroundColor={MID_GREEN} />
       )}
-      {/* <TouchableOpacity onPress={() => setRefresh(!refresh)}  >
-          <Text style={{ color: BLACK }}>PROGRESS CHECKER</Text>
-        </TouchableOpacity> */}
 
       <SafeAreaView style={{flex: 1, flexDirection: 'column'}}>
+        // Individual ProgressBar component
         {/* <ProgressBar
           images={images}
           onChange={() => {}}
           progressIndex={2}
           enableProgress={true}
           duration={20}
+          barStyle={{
+            barActiveColor: BAR_ACTIVE_COLOR,
+            barInActiveColor: BAR_INACTIVE_COLOR,
+            barWidth: 100, // always in number
+            barHeight: 4, // always in number
+          }}
         /> */}
+
+        // Individual Story View component
         <StoryContainer
           visible={true}
           enableProgress={false}
           images={images}
           duration={20}
+          containerStyle={{
+            width: '100%',
+            height: '100%',
+          }}
 
-          // User Information in header
+          // Inbuilt User Information in header
           userProfile={{
             userImage: PROFILE,
             userName: 'Yuvraj Pandey',
@@ -77,10 +91,10 @@ export default App = () => {
               Alert.alert('User profile image tapped');
             },
           }}
-          Custom header component option
+          // Custom Header component option
           // headerComponent={<View />}
 
-          Reply option in Footer
+          // Inbuilt Reply option  in Footer
           replyView={{
             isShowReply: true,
             onReplyTextChange: (textReply) => {
@@ -102,22 +116,18 @@ export default App = () => {
           // Custom Footer component option
           // footerComponent={<View />}
 
-          // containerStyle={{
-          //   width: '100%',
-          //   height: '100%',
-          // }}
-
+          // ProgressBar style options
           barStyle={{
             barActiveColor: BAR_ACTIVE_COLOR,
             barInActiveColor: BAR_INACTIVE_COLOR,
-            barWidth: 100,
-            barHeight: 4
+            barWidth: 100, // always in number
+            barHeight: 4, // always in number
           }}
 
+          // Story Image style options
           imageStyle={{
-            width: Dimensions.get('window').width,
-            height: Dimensions.get('window').height,
-            alignSelf: 'center',
+            width: Dimensions.get('window').width, // always in number
+            height: Dimensions.get('window').height, // always in number 
           }}
         />
       </SafeAreaView>
